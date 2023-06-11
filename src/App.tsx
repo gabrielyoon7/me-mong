630f3647e868d0578c13eda7b8c6d4c5daf5a53c
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-06-12 01:17:16 +0900

    feat: 메모 작성을 위한 기본적인 기능만 구현 해놓음

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState} from "react";
+import styled from "styled-components";
+
+export interface NewMemo {
+  title: string;
+  content: string;
+}
+
+// Styled Button component
+const Button = styled.button`
+  background-color: #3f51b5;
+  color: #ffffff;
+  padding: 8px 16px;
+  border: none;
+  border-radius: 4px;
+  font-size: 16px;
+  cursor: pointer;
+  margin-bottom: 16px;
+`;
+
+// Styled Input component
+const Input = styled.input`
+  padding: 8px 16px;
+  border: 1px solid #bdbdbd;
+  border-radius: 4px;
+  font-size: 16px;
+  margin-bottom: 16px;
+`;
+
+// Styled Textarea component
+const Textarea = styled.textarea`
+  padding: 8px 16px;
+  border: 1px solid #bdbdbd;
+  border-radius: 4px;
+  font-size: 16px;
+  margin-bottom: 16px;
+`;
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [newMemo, setNewMemo] = useState({
+    title: "",
+    content: ""
+  });
+
+  const handleNewMemo = () => {
+    // Implement the logic for handling the memo here
+    // For example, you can display the newMemo values in the console
+    console.log(newMemo);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Button onClick={handleNewMemo}>추가</Button>
+      <Input
+        value={newMemo.title}
+        onChange={(e) =>
+          setNewMemo((prevState) => ({
+            ...prevState,
+            title: e.target.value
+          }))
+        }
+      />
+      <Textarea
+        value={newMemo.content}
+        onChange={(e) =>
+          setNewMemo((prevState) => ({
+            ...prevState,
+            content: e.target.value
+          }))
+        }
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
