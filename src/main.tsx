81c519801fcd29903f626fb3a2d4f7fe6020c51b
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-06-17 15:55:58 +0900

    chore: msw 설치 및 기본 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,23 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import {worker} from "./mocks/browser.ts";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+async function main() {
+
+  await worker.start({
+    serviceWorker: {
+      url: '/me-mong/mockServiceWorker.js',
+    },
+    onUnhandledRequest: "bypass",
+  })
+
+  ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <React.StrictMode>
+      <App/>
+    </React.StrictMode>,
+  )
+}
+
+main();
