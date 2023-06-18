95b2cf3edd80d81973b92c2cbb380aac4ed9caa2
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-06-18 20:32:59 +0900

    feat: 노트 추가 로직을 msw에서도 진행하는 기능 구현

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,37 +1,60 @@
 // src/mocks/handlers.js
 import {rest} from 'msw'
+import {getLocalStorage, setLocalStorage} from "../utils/storage.ts";
+import {Note, NoteForm} from "../types/types.ts";
+import {LOCAL_STORAGE_KEY_NOTES} from "../api/keys.ts";
 
 export const handlers = [
-  rest.post('/login', (req, res, ctx) => {
-    // Persist user's authentication in the session
-    sessionStorage.setItem('is-authenticated', 'true')
+  // rest.post('/login', (req, res, ctx) => {
+  //   // Persist user's authentication in the session
+  //   sessionStorage.setItem('is-authenticated', 'true')
+  //
+  //   return res(
+  //     // Respond with a 200 status code
+  //     ctx.status(200),
+  //   )
+  // }),
+
+  // rest.get('/user', (req, res, ctx) => {
+  //   // Check if the user is authenticated in this session
+  //   const isAuthenticated = sessionStorage.getItem('is-authenticated')
+  //
+  //   if (!isAuthenticated) {
+  //     // If not authenticated, respond with a 403 error
+  //     return res(
+  //       ctx.status(403),
+  //       ctx.json({
+  //         errorMessage: 'Not authorized',
+  //       }),
+  //     )
+  //   }
+  //
+  //   // If authenticated, return a mocked user details
+  //   return res(
+  //     ctx.status(200),
+  //     ctx.json({
+  //       username: 'admin',
+  //     }),
+  //   )
+  // }),
+
+  rest.post('/memo/create', (req, res, ctx) => {
+    const body = req.body as string;
+    const newNoteForm: NoteForm = JSON.parse(body);
+
+    console.log(newNoteForm);
+    const prevNotes = getLocalStorage<Note[]>(LOCAL_STORAGE_KEY_NOTES, []);
+    const newNote: Note = {
+      id: prevNotes.length,
+      date: new Date().toDateString(),
+      ...newNoteForm
+    }
+    setLocalStorage<Note[]>(LOCAL_STORAGE_KEY_NOTES, [...prevNotes, newNote])
 
     return res(
       // Respond with a 200 status code
       ctx.status(200),
-    )
-  }),
-
-  rest.get('/user', (req, res, ctx) => {
-    // Check if the user is authenticated in this session
-    const isAuthenticated = sessionStorage.getItem('is-authenticated')
-
-    if (!isAuthenticated) {
-      // If not authenticated, respond with a 403 error
-      return res(
-        ctx.status(403),
-        ctx.json({
-          errorMessage: 'Not authorized',
-        }),
-      )
-    }
-
-    // If authenticated, return a mocked user details
-    return res(
-      ctx.status(200),
-      ctx.json({
-        username: 'admin',
-      }),
+      ctx.json('create')
     )
   }),
 ]
--- src/store/noteStore.ts
+++ src/store/noteStore.ts
@@ -5,24 +5,31 @@ let notes: Note[] = [];
 let listeners: Array<() => void> = [];
 
 export const noteStore = {
-  addNote(noteForm: NoteForm) {
+  addNote: async (noteForm: NoteForm) => {
+    const response = await fetch(`/memo/create`, {
+      method: 'POST',
+      body: JSON.stringify(noteForm)
+    });
+    const data = await response.json();
+    console.log(data);
+
     const newNote: Note = {
       id: nextId++,
       date: new Date().toDateString(),
       ...noteForm,
-    }
+    };
     notes = [...notes, newNote];
     emitChange();
   },
-  subscribe(listener: () => void) {
+  subscribe: (listener: () => void) => {
     listeners = [...listeners, listener];
     return () => {
-      listeners = listeners.filter(l => l !== listener);
+      listeners = listeners.filter((l) => l !== listener);
     };
   },
-  getSnapshot() {
+  getSnapshot: () => {
     return notes;
-  }
+  },
 };
 
 function emitChange() {
