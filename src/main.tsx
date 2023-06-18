48e43c904ca1ac19fdea4868b41585308f72d2f2
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-06-19 00:36:24 +0900

    chore: ReactQueryDevtools 설치

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { worker } from "./mocks/browser.ts";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient()
 
@@ -20,6 +21,11 @@ async function main() {
     <React.StrictMode>
       <QueryClientProvider client={queryClient}>
         <App />
+        <ReactQueryDevtools
+          initialIsOpen={false}
+          position="bottom-left"
+          panelPosition="top"
+        />
       </QueryClientProvider>
     </React.StrictMode>,
   )
